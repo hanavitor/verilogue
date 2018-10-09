722cb56702ecc7016a59c1aa13a2acdb16e967c9
hanaelvitor <37921454+hanavitor@users.noreply.github.com>
2018-10-09 14:51:05 -0300

    Update verilogue.v

--- verilogue.v
+++ verilogue.v
@@ -1,1 +1,21 @@
+module projetao(botao,led, clk);
 
+input botao, clk;
+output reg led;
+reg estado;
+reg [31:0] contador = 0;
+
+always@(botao)begin
+	if(botao == 0)begin
+		led = estado;
+	end else begin
+		led = 0;
+	end
+end
+
+always@(posedge clk)begin
+	contador = contador + 1;
+	estado = contador[20];
+end
+
+endmodule 
